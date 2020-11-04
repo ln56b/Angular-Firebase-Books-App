a5c0f36fed415b0ff28a5a633377c285b319a855
Hélène Baudrand <baudrand.helene@gmail.com>
2020-11-04 11:14:40 +0100

    connected firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import firebase from "@firebase/app";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'angular-firebase-books-app';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyBuTDXuGpTiDMgsJMiA_N5FvltzvEmL9PI",
+      authDomain: "angular-books-3ab1e.firebaseapp.com",
+      databaseURL: "https://angular-books-3ab1e.firebaseio.com",
+      projectId: "angular-books-3ab1e",
+      storageBucket: "angular-books-3ab1e.appspot.com",
+      messagingSenderId: "639984960755",
+      appId: "1:639984960755:web:5eaabfe43fc18d9bc0e386",
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
