21f598e881a4758dc2129df65b50856c5f052c58
Hélène Baudrand <baudrand.helene@gmail.com>
2020-11-04 11:08:40 +0100

    initialized routes and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,36 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { SigninComponent } from "./auth/signin/signin.component";
+import { SignupComponent } from "./auth/signup/signup.component";
+import { BookFormComponent } from "./book-list/book-form/book-form.component";
+import { BookListComponent } from "./book-list/book-list.component";
+import { SingleBookComponent } from "./book-list/single-book/single-book.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "auth/signup",
+    component: SignupComponent,
+  },
+  {
+    path: "auth/signin",
+    component: SigninComponent,
+  },
+  {
+    path: "books",
+    component: BookListComponent,
+  },
+  {
+    path: "books/new",
+    component: BookFormComponent,
+  },
+  {
+    path: "books/view/:id",
+    component: SingleBookComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
-import { BookListComponent } from './book-list/book-list.component';
-import { SingleBookComponent } from './book-list/single-book/single-book.component';
-import { BookFormComponent } from './book-list/book-form/book-form.component';
-import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SignupComponent } from "./auth/signup/signup.component";
+import { SigninComponent } from "./auth/signin/signin.component";
+import { BookListComponent } from "./book-list/book-list.component";
+import { SingleBookComponent } from "./book-list/single-book/single-book.component";
+import { BookFormComponent } from "./book-list/book-form/book-form.component";
+import { HeaderComponent } from "./header/header.component";
+import { AuthService } from "./services/auth.service";
+import { BooksService } from "./services/books.service";
+import { AuthGuardService } from "./services/auth-guard.service";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -18,13 +23,16 @@ import { HeaderComponent } from './header/header.component';
     BookListComponent,
     SingleBookComponent,
     BookFormComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AuthService, BooksService, AuthGuardService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
