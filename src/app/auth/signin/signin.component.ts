32fa83d7c388c7e2daa238eb4a91f81b5f2c1bd5
Hélène Baudrand <baudrand.helene@gmail.com>
2020-11-04 11:46:24 +0100

    created auth components

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-signin',
-  templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+  selector: "app-signin",
+  templateUrl: "./signin.component.html",
+  styleUrls: ["./signin.component.scss"],
 })
 export class SigninComponent implements OnInit {
+  formGroup: FormGroup;
+  errorMessage: string;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm() {
+    this.formGroup = this.fb.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: [
+        "",
+        [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)],
+      ],
+    });
+  }
+
+  onSubmit() {
+    const email = this.formGroup.get("email").value;
+    const password = this.formGroup.get("password").value;
+    this.authService.signInUser(email, password).then(
+      () => {
+        this.router.navigate(["/books"]);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.scss"],
 })
 export class SignupComponent implements OnInit {
+  formGroup: FormGroup;
+  errorMessage: string;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm() {
+    this.formGroup = this.fb.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: [
+        "",
+        [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)],
+      ],
+    });
+  }
+
+  onSubmit() {
+    const email = this.formGroup.get("email").value;
+    const password = this.formGroup.get("password").value;
+    this.authService.createNewUser(email, password).then(
+      () => {
+        this.router.navigate(["/books"]);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
